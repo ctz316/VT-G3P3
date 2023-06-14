21faebe69c169f221958d8f064bbbd7cd11cb956
Nick S <112427784+zaknk@users.noreply.github.com>
2023-06-14 17:35:41 -0400

    Update InventoryRepository.java
    removed comment

--- src/main/java/com/skillstorm/project3/repositories/InventoryRepository.java
+++ src/main/java/com/skillstorm/project3/repositories/InventoryRepository.java
@@ -10,6 +10,5 @@ import com.skillstorm.project3.models.Inventory;
 public interface InventoryRepository extends CrudRepository<Inventory, Integer>{
 	
 	@Query(value = "SELECT * FROM inventory WHERE warehouse_id = :whId", nativeQuery = true)
-	// declare your method; this is an interface , so we don't include implementation
 	public Iterable<Inventory> findByWarehouseId(int whId);
 }
