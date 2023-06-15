9ff4e0d4fdff35faf8eb3ffc441fda57bd1d5791
Kyle Cook <75640815+kylecook-cs@users.noreply.github.com>
2023-06-14 19:10:06 -0500

    Update to Size
    Update Size repo, class, model, service

--- src/main/java/com/skillstorm/project3/controllers/SizeController.java
+++ src/main/java/com/skillstorm/project3/controllers/SizeController.java
@@ -1,11 +1,18 @@
 package com.skillstorm.project3.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.skillstorm.project3.models.Size;
 import com.skillstorm.project3.services.SizeService;
 
 @RestController
@@ -14,11 +21,26 @@ import com.skillstorm.project3.services.SizeService;
 public class SizeController {
 
     @Autowired
-    private SizeService service;
+    SizeService sizeService;
 
-    @GetMapping
-    public void test() {
-        System.out.println("Test");
+    @GetMapping("/all")
+    public Iterable<Size> getAllSizes() {
+        return sizeService.getAllSizes();
     }
-
+    
+    @GetMapping("/{id}")
+    public Size getSizeById(@PathVariable int id) {
+    	return sizeService.getSizeById(id);
+    }
+    
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<String> addSize(@RequestBody Size size) {
+    	return sizeService.addSize(size);
+    }
+    
+    @DeleteMapping
+    public ResponseEntity<String> deleteSize(int id) {
+		return sizeService.deleteSizeById(id);
+	}
 }
--- src/main/java/com/skillstorm/project3/models/Size.java
+++ src/main/java/com/skillstorm/project3/models/Size.java
@@ -12,34 +12,35 @@ public class Size {
     private int sizeId;
     @Column(name = "size_name")
     private String sizeName;
-
-    // Constructors
+    
     public Size() {}
-    public Size(int sizeId, String sizeName) {
-        this.sizeId = sizeId;
-        this.sizeName = sizeName;
-    }
+    
+	public Size(int sizeId, String sizeName) {
+		this.sizeId = sizeId;
+		this.sizeName = sizeName;
+	}
 
-    // To String method
-    @Override
-    public String toString() {
-        return "Sizes{" +
-                "sizeId=" + sizeId +
-                ", sizeName='" + sizeName + '\'' +
-                '}';
-    }
+	public int getSizeId() {
+		return sizeId;
+	}
 
-    // Getters and Setters
-    public int getSizeId() {
-        return sizeId;
-    }
-    public void setSizeId(int sizeId) {
-        this.sizeId = sizeId;
-    }
-    public String getSizeName() {
-        return sizeName;
-    }
-    public void setSizeName(String sizeName) {
-        this.sizeName = sizeName;
-    }
+	public void setSizeId(int sizeId) {
+		this.sizeId = sizeId;
+	}
+
+	public String getSizeName() {
+		return sizeName;
+	}
+
+	public void setSizeName(String sizeName) {
+		this.sizeName = sizeName;
+	}
+
+	@Override
+	public String toString() {
+		return "Size [sizeId=" + sizeId + ", sizeName=" + sizeName + "]";
+	}
+	
 }
+
+    
