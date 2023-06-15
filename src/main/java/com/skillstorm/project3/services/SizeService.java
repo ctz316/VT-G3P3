38deb61dc5e04c36c62a8a46b89a30302defdd3b
Kyle Cook <75640815+kylecook-cs@users.noreply.github.com>
2023-06-14 22:18:10 -0500

    Updated Caliber and Warehouse Service/Controller
    updated all caliber files and all warehouse files. as well as quick update to size

--- src/main/java/com/skillstorm/project3/services/SizeService.java
+++ src/main/java/com/skillstorm/project3/services/SizeService.java
@@ -1,8 +1,6 @@
 package com.skillstorm.project3.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.skillstorm.project3.models.Size;
 import com.skillstorm.project3.repositories.SizeRepository;
