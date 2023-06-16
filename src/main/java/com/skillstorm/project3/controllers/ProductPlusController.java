fb49155a8159d263b185c3e1afa2bbf7c7088ba9
Nick S <112427784+zaknk@users.noreply.github.com>
2023-06-16 09:08:17 -0400

    Update ProductPlusController.java

--- src/main/java/com/skillstorm/project3/controllers/ProductPlusController.java
+++ src/main/java/com/skillstorm/project3/controllers/ProductPlusController.java
@@ -1,21 +1,25 @@
 package com.skillstorm.project3.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skillstorm.project3.models.Caliber;
 import com.skillstorm.project3.models.Category;
+import com.skillstorm.project3.models.Manufacturer;
 import com.skillstorm.project3.models.Product;
 import com.skillstorm.project3.models.Size;
 import com.skillstorm.project3.repositories.CategoryRepository;
+import com.skillstorm.project3.repositories.ManufacturerRepository;
 import com.skillstorm.project3.services.CaliberService;
 import com.skillstorm.project3.services.ProductService;
 import com.skillstorm.project3.services.SizeService;
 
 @RestController
 @RequestMapping("/p")
+@CrossOrigin(origins = "*")
 public class ProductPlusController {
 	
 	@Autowired
@@ -30,8 +34,12 @@ public class ProductPlusController {
 	@Autowired
 	private CategoryRepository catRepo;
 	
+	@Autowired
+	private ManufacturerRepository manuRepo;
+	
 	@GetMapping("/all")
 	public Iterable<Product> getAllProducts() {
+		System.out.println("in p all" + prodServ.getAllProducts());
 		return prodServ.getAllProducts();
 	}
 	
@@ -48,5 +56,10 @@ public class ProductPlusController {
 	@GetMapping("/cats/all")
 	public Iterable<Category> getAllCategories() {
 		return catRepo.findAll();
+	}
+	
+	@GetMapping("/manus/all")
+	public Iterable<Manufacturer> getAllManufacturers() {
+		return manuRepo.findAll();
 	}
 }
